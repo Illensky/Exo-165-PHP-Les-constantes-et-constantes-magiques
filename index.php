8e9351111642cbd575445dd9e150b10d4b3d41f8
Illensky <alexis.laroche.02240@gmail.com>
2022-01-24 11:34:43 +0100

    Exercice done

--- index.php
+++ index.php
@@ -9,9 +9,20 @@
  *    longueur de la chaîne de la troisième constante ( attention aux priorités )
  */
 // TODO Votre code ici.
-
+define('CONST1', 12);
+define('CONST2', 2);
+echo CONST1*CONST2;
+define('CONST3', "C'est COOL PHP");
+echo (CONST1+CONST2)*strlen(CONST3);
 
 /**
  * 6. Utilisez la bonne constante magique et éventuellement la bonne fonction pour afficher un maximum d'informations sur la page actuelle4
  */
 // TODO Votre code ici.
+
+echo "<pre>";
+echo __DIR__;
+echo __FILE__;
+echo __LINE__;
+echo "</pre>";
+
